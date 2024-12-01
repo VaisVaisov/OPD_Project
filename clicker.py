c24fdd38d75557499bde906e3d08d6e39ac02dbb
vais55 <vais55.va@gmail.com>
2024-12-01 15:25:13 +0500

    some update

--- clicker.py
+++ clicker.py
@@ -7,14 +7,17 @@ height_screen = 600
 screen = pygame.display.set_mode((width_screen, height_screen))
 
 background = pygame.image.load('background.jpg')
+button_not_clicked_image = pygame.image.load('button_not_clicked.png')
+button_clicked_image = pygame.image.load('button_clicked.png')
+
 
 class Button():
-    def __init__(self):
-        self.button_image = pygame.image.load('button.png')
+    def __init__(self, image):
+        self.image = image
         self.x = width_screen // 3
         self.y = height_screen // 2.5
-        self.width = self.button_image.get_width() // 1.2
-        self.height = self.button_image.get_height() // 1.8
+        self.width = self.image.get_width() // 1.2
+        self.height = self.image.get_height() // 1.8
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
 score_font = pygame.font.SysFont('comicsansms', 40)
@@ -26,29 +29,32 @@ def show_scores():
 
 run_game = True
 
-sck=0.3
+scores_multiplier = 0.3
 start_time = 0
 end_time = 0
 
+button = Button(button_not_clicked_image)
+
 while run_game:
     screen.blit(background, (0, 0))
-    button = Button()
-    screen.blit(button.button_image, (width_screen // 3.3, height_screen // 3.3))
+    screen.blit(button.image, (width_screen // 3.3, height_screen // 3.3))
     show_scores()
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if button.hitbox.collidepoint(event.pos):
                 start_time = time.time()
+                button.image = button_clicked_image
             else:
                 start_time = 0
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             if button.hitbox.collidepoint(event.pos):
                 end_time = time.time()
+                button.image = button_not_clicked_image
             else:
                 end_time = 0
             time_passed = (end_time - start_time) * 100
             print(time_passed)
-            scores = round(scores + time_passed*sck)
+            scores = round(scores + time_passed * scores_multiplier)
         if event.type == pygame.QUIT:
             run_game = False
     pygame.display.flip()
