e85ba4e324759430c98221b79e81202fe0eebd9a
vais55 <vais55.va@gmail.com>
2024-12-01 16:57:32 +0500

    it's working

--- clicker.py
+++ clicker.py
@@ -1,5 +1,4 @@
 import pygame, sys, time
-from pygame.locals import *
 pygame.init()
 
 width_screen = 800
@@ -19,6 +18,7 @@ class Button():
         self.y = height_screen // 2.5
         self.width = self.image.get_width() // 1.2
         self.height = self.image.get_height() // 1.8
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
 score_font = pygame.font.SysFont('comicsansms', 40)
 scores = 0
@@ -32,6 +32,7 @@ run_game = True
 scores_multiplier = 0.3
 start_time = 0
 end_time = 0
+button_pressed = False
 
 button = Button(button_not_clicked_image)
 
@@ -40,18 +41,18 @@ while run_game:
     screen.blit(button.image, (width_screen // 3.3, height_screen // 3.3))
     show_scores()
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == K_SPACE:
-            start_time = time.time()
-            button.image = button_clicked_image
-        else:
-            start_time = 0
-        if event.type == pygame.KEYUP and event.key == K_SPACE:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if button.hitbox.collidepoint(event.pos):
+                start_time = time.time()
+                button.image = button_clicked_image
+                button_pressed = True
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and button_pressed:
             end_time = time.time()
             button.image = button_not_clicked_image
-        else:
-            end_time = 0
-        time_passed = (end_time - start_time) * 100
-        scores = round(scores + time_passed * scores_multiplier)
+            button_pressed = False
+            time_passed = (end_time - start_time) * 100
+            print(time_passed)
+            scores = round(scores + time_passed * scores_multiplier)
         if event.type == pygame.QUIT:
             run_game = False
     pygame.display.flip()
