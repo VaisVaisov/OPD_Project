0fe87ef001fafc344369c176169f7f1b667147fa
vais55 <vais55.va@gmail.com>
2024-12-01 14:06:39 +0500

    bug fixed

--- clicker.py
+++ clicker.py
@@ -11,10 +11,10 @@ background = pygame.image.load('background.jpg')
 class Button():
     def __init__(self):
         self.button_image = pygame.image.load('button.png')
-        self.x = width_screen // 3.3
-        self.y = height_screen // 3.3
-        self.width = self.button_image.get_width()
-        self.height = self.button_image.get_height()
+        self.x = width_screen // 3
+        self.y = height_screen // 2.5
+        self.width = self.button_image.get_width() // 1.2
+        self.height = self.button_image.get_height() // 1.8
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
 score_font = pygame.font.SysFont('comicsansms', 40)
@@ -27,6 +27,8 @@ def show_scores():
 run_game = True
 
 sck=0.3
+start_time = 0
+end_time = 0
 
 while run_game:
     screen.blit(background, (0, 0))
@@ -37,8 +39,13 @@ while run_game:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if button.hitbox.collidepoint(event.pos):
                 start_time = time.time()
+            else:
+                start_time = 0
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            end_time = time.time()
+            if button.hitbox.collidepoint(event.pos):
+                end_time = time.time()
+            else:
+                end_time = 0
             time_passed = (end_time - start_time) * 100
             print(time_passed)
             scores = round(scores + time_passed*sck)
